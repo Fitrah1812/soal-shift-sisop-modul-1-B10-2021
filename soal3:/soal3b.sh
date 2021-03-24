ef39fd37e6241547e19151d4d0e3b978c4f8c85d
yusrilz <detechtive@DeTechTive-PC.localdomain>
2021-03-24 22:55:55 +0700

    Add soal3b

---
+++ soal3:/soal3b.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/soal-shift-sisop-modul-1-B10-2021/soal3:
+bash ./soal3a.sh
+now=$(date +"%d-%m-%Y")
+echo "$now"
+mkdir "$now"
+
+mv ./Koleksi_* "./$now/"
+mv ./foto.log "./$now/"
+
