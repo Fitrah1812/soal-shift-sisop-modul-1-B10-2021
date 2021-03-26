2d0f6817b982cb344cc5eb65e72e357f68693695
Fitrah Arie Ramadhan <56785130+Fitrah1812@users.noreply.github.com>
2021-03-26 21:58:38 +0700

    Update Final fix error

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,29 +1,21 @@
 #!bin/bash
 
 #1A Tampilkan Error jumlah dan info jumlah
-sinfo="$(grep -E -o "INFO" syslog.log)"
-serror="$(grep -E -o "ERROR" syslog.log)"
-jinfo="$(grep -o "INFO" syslog.log | wc -l)"
-jerror="$(grep -o "ERROR" syslog.log | wc -l)"
-#echo $jinfo >> data.csv;
-#echo $jerror >> data.csv;
+allerrorinfo="$(grep -o 'y:.*' syslog.log | cut -f2- -d\ )"
+#echo $allerror > alreror.csv
 
 #1BTampilkan Error Beserta jumlahnya
-perror=`grep -o 'ERROR.*' syslog.log | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
-pinfo=`grep -o 'INFO.*' syslog.log | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
+perror=`printf "$allerrorinfo" | grep -o 'ERROR.*' | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
+pinfo=`printf "$allerrorinfo" | grep -o 'INFO.*' syslog.log | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
 #1C
 #uniq -> select distinct
 usertag=`cut -d"(" -f2 < syslog.log | cut -d")" -f1 | sort | uniq`
-#1D
-#Cara awal manual
-#update="$(grep -o "The ticket was modified while updating" syslog.log | wc -l)"
-#gagal="$(grep -o "Permission denied while closing ticket" syslog.log | wc -l)"
-#tambah="$(grep -o "Tried to add information to closed ticket" syslog.log | wc -l)"
-#time="$(grep -o "Timeout while retrieving information" syslog.log | wc -l)"
-#tikethabis="$(grep -o "Ticket doesn't exist" syslog.log | wc -l)"
-#gagaldb="$(grep -o "Connection to DB failed" syslog.log | wc -l)"
-#printf "The ticket was modified while updating,%d\nPermission denied while closing ticket,%d\nTried to add information to closed ticket,%d\nTimeout while retrieving information,%d\nTicket doesn't exist,%d\nConnection to DB failed,%d\n" $update $gagal $tambah $time $tikethabis $gagaldb | sort -t ',' -k2 -n -r | sed '1 i\'$teks > error_message.csv
+usererror=`printf "$allerrorinfo" | grep -o 'ERROR.*' | cut -f2- -d"(" | cut -d")" -f 1 | sort -V | uniq -c`
+userinfo=`printf "$allerrorinfo" | grep -o 'INFO.*' syslog.log | cut -f2- -d"(" | cut -d")" -f 1 | sort -V | uniq -c`
+#echo "$usererror" > usererror.csv
+#echo "$userinfo" > infoerror.csv
 
+#1D
 #cara1D yang benar
 teks="Error,Count";
 echo "$perror" | while read cekerror
@@ -35,16 +27,14 @@ done | sed '1 i\'$teks > error_message.csv
 
 
 #cara kedua
-#echo $update, $gagal, $tambah, $time, $tikethabis, $gagal > error_message.csv
-#echo "Error,Count" >> error_message.csv
-#echo "The ticket was modified while updating,$update" >> error_message.csv
-#echo "Permission denied while closing ticket,$gagal" >> error_message.csv
-#echo "Tried to add information to closed ticket,$tambah" >> error_message.csv
-#echo "Timeout while retrieving information,$time" >> error_message.csv
-#echo "Ticket doesn't exist,$tikethabis" >> error_message.csv
-#echo "Connection to DB failed,$gagaldb" >> error_message.csv
-#sort -k 2 -t , error_message.csv >> error_message.csv
-#sort -t, -k2 -n error_message.csv >> error_message.csv
+#Cara awal manual
+#update="$(grep -o "The ticket was modified while updating" syslog.log | wc -l)"
+#gagal="$(grep -o "Permission denied while closing ticket" syslog.log | wc -l)"
+#tambah="$(grep -o "Tried to add information to closed ticket" syslog.log | wc -l)"
+#time="$(grep -o "Timeout while retrieving information" syslog.log | wc -l)"
+#tikethabis="$(grep -o "Ticket doesn't exist" syslog.log | wc -l)"
+#gagaldb="$(grep -o "Connection to DB failed" syslog.log | wc -l)"
+#printf "The ticket was modified while updating,%d\nPermission denied while closing ticket,%d\nTried to add information to closed ticket,%d\nTimeout while retrieving information,%d\nTicket doesn't exist,%d\nConnection to DB failed,%d\n" $update $gagal $tambah $time $tikethabis $gagaldb | sort -t ',' -k2 -n -r | sed '1 i\'$teks > error_message.csv
 
 #1E
 teks2="Username,INFO,ERROR"
@@ -55,6 +45,3 @@ while read sum
         jumlaherror=$(grep -E "ERROR.*($sum))" syslog.log | wc -l)
         echo "$sum,$jumlahinfo,$jumlaherror"
     done | sed '1 i\'$teks2 > user_statistic.csv;
-
-
-
