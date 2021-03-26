e97a7a50f5317de68a0bfd20baab21ed89bf1a67
Fitrah Arie Ramadhan <56785130+Fitrah1812@users.noreply.github.com>
2021-03-26 19:15:31 +0700

    Final Berbagai cara

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -9,22 +9,23 @@ jerror="$(grep -o "ERROR" syslog.log | wc -l)"
 #echo $jerror >> data.csv;
 
 #1BTampilkan Error Beserta jumlahnya
-perror=`printf "$serror" | grep -o 'ERROR.*' syslog.log | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
-pinfo=`printf "$sinfo" | grep -o 'ERROR.*' syslog.log | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
+perror=`grep -o 'ERROR.*' syslog.log | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
+pinfo=`grep -o 'INFO.*' syslog.log | cut -f2- -d\ | cut -d"(" -f 1 | sort -V | uniq -c | sort -n -r`
 #1C
 #uniq -> select distinct
 usertag=`cut -d"(" -f2 < syslog.log | cut -d")" -f1 | sort | uniq`
 #1D
+#Cara awal manual
 #update="$(grep -o "The ticket was modified while updating" syslog.log | wc -l)"
 #gagal="$(grep -o "Permission denied while closing ticket" syslog.log | wc -l)"
 #tambah="$(grep -o "Tried to add information to closed ticket" syslog.log | wc -l)"
 #time="$(grep -o "Timeout while retrieving information" syslog.log | wc -l)"
 #tikethabis="$(grep -o "Ticket doesn't exist" syslog.log | wc -l)"
 #gagaldb="$(grep -o "Connection to DB failed" syslog.log | wc -l)"
-
-teks="Error_Count";
 #printf "The ticket was modified while updating,%d\nPermission denied while closing ticket,%d\nTried to add information to closed ticket,%d\nTimeout while retrieving information,%d\nTicket doesn't exist,%d\nConnection to DB failed,%d\n" $update $gagal $tambah $time $tikethabis $gagaldb | sort -t ',' -k2 -n -r | sed '1 i\'$teks > error_message.csv
 
+#cara1D yang benar
+teks="Error,Count";
 echo "$perror" | while read cekerror
 do
     jumlaherror=`echo $cekerror | cut -d ' ' -f 1`
@@ -46,14 +47,14 @@ done | sed '1 i\'$teks > error_message.csv
 #sort -t, -k2 -n error_message.csv >> error_message.csv
 
 #1E
-
+teks2="Username,INFO,ERROR"
 printf "$usertag" | 
 while read sum
     do
         jumlahinfo=$(grep -E "INFO.*($sum))" syslog.log | wc -l)
         jumlaherror=$(grep -E "ERROR.*($sum))" syslog.log | wc -l)
         echo "$sum,$jumlahinfo,$jumlaherror"
-    done | sed '1 i\Username,INFO,ERROR' > user_statistic.csv;
+    done | sed '1 i\'$teks2 > user_statistic.csv;
 
 
 
